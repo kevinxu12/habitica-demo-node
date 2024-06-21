114dcb8aa9a704d0fe95bdffa4aeacab1679965e
Alexander Dines <alex@runloop.ai>
2024-06-21 12:05:05 -0700

    cp dines

---
+++ README.md
@@ -0,0 +1,37 @@
+# Habitica_node
+
+A demo node app for Runloop Node lambda functions and devboxes. The NPM package with type definitions is available [@runloop/runloop](https://www.npmjs.com/package/@runloop/runloop).
+
+The demo performs the following
+
+- Clones Habitica
+- NPM install
+- Uses file api
+- Lints the repo
+
+See [platform.runloop.ai](https://platform.runloop.ai) to setup your accunt and inspect deployments, invocations, and devbox logs.
+
+## Trigger an invocation of a RL function
+
+```bash
+curl -X 'POST' \
+  'https://api.runloop.ai/v1/functions/habitica-demo/useDevbox/invoke_async' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' -H 'Authorization: Bearer ak_XXX' \
+  -d '{
+  "request": {}
+}'
+> {"id":"inv_2wuHDGq6CnStti0wf4LPI","function_name":"useDevbox","project_name":"habitica-demo","status":"running","result":null,"error":null}
+```
+
+## Get the status of an invocation
+
+```bash
+curl -X 'GET' \
+  'https://api.runloop.ai/v1/functions/invocations/<id from above>' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' -H 'Authorization: Bearer ak_XXX' \
+  -d '{
+  "request": {}
+}'
+```
---
+++ runloop/devboxTest.ts
@@ -0,0 +1,33 @@
+import { RLFunc } from "@runloop/runloop";
+
+let gh_token = process.env.GH_TOKEN;
+
+export const useDevbox = RLFunc({
+  id: "useDevbox",
+  run: async function (request, { systemCoordinator }) {
+    console.log(`TOKEN: ${gh_token}`);
+    let devbox = await systemCoordinator.createDevbox(undefined, undefined, [
+      "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -",
+      "sudo apt install -y --no-install-recommends nodejs libkrb5-dev",
+    ]);
+    console.log("Devbox created: ", devbox.id);
+
+    await devbox.execTool.exec(
+      `GH_TOKEN=${gh_token} gh repo clone HabitRPG/habitica ./code -- --depth=1`
+    );
+    await devbox.execTool.exec("cd code && ls -la");
+    await devbox.execTool.exec("cd code && npm i");
+
+    console.log("File tools Create");
+    await devbox.fileTool.createFile("test.txt", "Hello World");
+
+    console.log("File tools read");
+    let readResult = await devbox.fileTool.readFile("test.txt");
+
+    console.log(`File Read result: ${readResult}`);
+    let result = await devbox.execTool.exec("cd code && npm run lint");
+    console.log(`Lint result${result.stdout}`);
+
+    return { "readresult:": readResult, lintResult: result.stdout };
+  },
+});
